76e11c27fbe70ac71f591f08db280fb4eaf93196
Esteban Molina <solsel90@gmail.com>
2023-03-06 19:28:58 -0300

    se resolvio el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let numA = parseInt(prompt('Ingrese el primer numero'));
+let numB = parseInt(prompt('Ingrese el segundo numero'));
+
+if (numA > numB){
+    document.write((numA) + ' es mayor que ' + (numB));
+} else {
+    document.write((numB) + ' es mayor que ' + (numA));
+}
